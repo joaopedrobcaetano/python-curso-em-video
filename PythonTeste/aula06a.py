8f749128d569609413db268223d08c454dc403b2
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-01-08 11:05:19 -0300

    testes aula 06

---
+++ PythonTeste/aula06a.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor: '))
+print(type(n1))
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+soma = num1 + num2
+print('A soma entre {} e {} é {}.'.format(num1, num2, soma))
---
+++ PythonTeste/aula06b.py
@@ -0,0 +1,11 @@
+n1 = input('Digite um valor: ')
+print('Este valor é numérico?' )
+if n1.isnumeric():
+    print('Sim')
+else:
+    print('Não')
+print('Este valor é alfabético?')
+if n1.isalpha():
+    print('Sim')
+else:
+    print('Não')
