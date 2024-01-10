2e3f39cc257dae693ee552e6cb292735d619bf0c
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-01-10 14:57:13 -0300

    atualizando exercício 08

--- PythonExercicios/ex008.py
+++ PythonExercicios/ex008.py
@@ -1,4 +1,4 @@
 mValor = float(input('Digite o valor em metros: '))
 cmValor = mValor * 100
 mmValor = mValor * 1000
-print('Valor em metros: {} m\nValor em centímetros: {} cm\nValor em milímetros: {} mm'.format(mValor, cmValor, mmValor))
+print('Valor em metros: {} m\nValor em centímetros: {:.0f} cm\nValor em milímetros: {:.0f} mm'.format(mValor, cmValor, mmValor))
