2f3bd09d24e73c1c976dc4e443ff5c05ce01d4cb
joaopedrobcaetano <joaopedrobcaetano@gmail.com>
2024-01-10 15:18:39 -0300

    atualizando exercícios 09 e 10

--- PythonExercicios/ex010.py
+++ PythonExercicios/ex010.py
@@ -1,3 +1,3 @@
 reais = float(input('Quantos reais você tem? R$ '))
-dolares = reais * 4.89
-print('Você pode comprar U$$ {:.2f}.'.format(dolares))
+dolares = reais / 4.89
+print('Com R$ {:.2f}, você pode comprar U$$ {:.2f}.'.format(reais, dolares))
